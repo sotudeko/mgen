874455ca2078e766b71f40d84cbf701b1612492b
Sola Otudeko <sotudeko@sonatype.com>
2021-12-09 09:25:32 +0000

    17

--- src/main/java/org/so/example/mgen/util/FilenameInfo.java
+++ src/main/java/org/so/example/mgen/util/FilenameInfo.java
@@ -18,13 +18,13 @@ public class FilenameInfo {
     public static final String[] autoReleasedFromQuarantineComponentsFileHeader = {"Application Name", "Evaluation Date", "Stage"};
 
     public static final String autoReleasedFromQuarantineConfigCsvFile = "autoReleasedFromQuarantineConfig.csv";
-    public static final String[] autoReleasedFromQuarantineConfigFileHeader = {"Id", "Name"};
+    public static final String[] autoReleasedFromQuarantineConfigFileHeader = {"Config Id", "Name", "Enabled"};
 
     public static final String quarantinedComponentsSummaryCsvFile = "quarantinedComponentsSummary.csv";
-    public static final String[] quarantinedComponentsSummaryFileHeader = {"Id", "Name"};
+    public static final String[] quarantinedComponentsSummaryFileHeader = {"Repository Count", "Quarantine Enabled Count", "Quarantine Enabled", "Total Component Count", "Quarantined Component Count"};
 
     public static final String autoReleasedFromQuarantineSummaryCsvFile = "autoReleasedFromQuarantineSummary.csv";
-    public static final String[] autoReleasedFromQuarantineSummaryFileHeader = {"Id", "Name"};
+    public static final String[] autoReleasedFromQuarantineSummaryFileHeader = {"MTD", "YTD"};
 
     public static final String organizationsCsvFile = "organizations.csv";
     public static final String[] organizationsFileHeader = {"Id", "Name"};
