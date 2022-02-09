e6e76e4547e1b71f469c578d4c0400b86c3c1660
Sola <solao@novi.local>
2022-02-09 19:33:03 +0000

    65

--- releasefiles/runapp-docker.sh
+++ releasefiles/runapp-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+ts=$(date +"%d%m%Y:%H%M%S")
 workdir=$(pwd)
-metricsdir=${workdir}/metrics
+metricsdir=${workdir}/metrics-${ts}
 
 docker run --rm -it -v ${workdir}:/config -v ${metricsdir}:/metrics ghcr.io/sotudeko/mgen:@APPVER@
 
