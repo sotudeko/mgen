8dd5da670894a5a423f15f6c774ab9125538cd40
Sola <solao@novi.local>
2022-02-10 09:29:59 +0000

    68

--- releasefiles/runapp-docker.sh
+++ releasefiles/runapp-docker.sh
@@ -2,7 +2,8 @@
 
 ts=$(date +"%d%m%Y:%H%M%S")
 workdir=$(pwd)
-metricsdir=${workdir}/iqmetrics-${ts}
+#metricsdir=${workdir}/iqmetrics-${ts}
+metricsdir=${workdir}/iqmetrics
 
 docker run --rm -it -v ${workdir}:/config -v ${metricsdir}:/iqmetrics ghcr.io/sotudeko/mgen:@APPVER@
 
